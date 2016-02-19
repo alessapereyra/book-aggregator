1b6485d5f0c8c7fde524d6edfb7782aa148c9c02
Alvaro Pereyra <alvaro@xendacentral.com>
2016-02-19 12:23:38 -0500

    Fixes bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @bookstores = [Amazon,Mondadori,Overdrive,BarnesAndNoble,KokoBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata,TodoEbook]
+    @bookstores = [Amazon,Mondadori,Overdrive,BarnesAndNoble,KoboBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata,TodoEbook]
   end
 
   def search
