d60bb93a26ab74eac929902ca449b7a99099669b
Sascha Reinstein <sascha206@hotmail.com>
2016-03-08 20:27:01 +0000

    Added AmazonMX support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
+  
+  SUPPORTED_BOOKSTORES = [Amazon,AmazonMX,Mondadori,Overdrive,BarnesAndNoble,KoboBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata,TodoEbook]
+  #SUPPORTED_BOOKSTORES = [AmazonMX]
+  
   def index
-    @bookstores = [Amazon,Mondadori,Overdrive,BarnesAndNoble,KoboBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata,TodoEbook]
+    @bookstores = SUPPORTED_BOOKSTORES
   end
 
   def search
-    #BookShout,TheCopia,Indigo,Chegg,BakerAndTaylor, Sainsbury, fnac (POST), skoobe (menu JS), readcloud (POST), Leer-e
-    #Scribd ? Gardners (need ISBN)
-    #Amazon,Mondadori,Overdrive,BarnesAndNoble,KokoBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata
-    @bookstores = [Amazon,Mondadori,Overdrive,BarnesAndNoble,KoboBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata,TodoEbook]
-    #@bookstores = [Overdrive,KokoBooks,Wheelers]
+    @bookstores = SUPPORTED_BOOKSTORES
     render :index
   end
 
@@ -17,3 +17,7 @@ class HomeController < ApplicationController
     @content = @klass.render
   end
 end
+
+#BookShout,TheCopia,Indigo,Chegg,BakerAndTaylor, Sainsbury, fnac (POST), skoobe (menu JS), readcloud (POST), Leer-e
+#Scribd ? Gardners (need ISBN)
+#Amazon,Mondadori,Overdrive,BarnesAndNoble,KokoBooks,Wheelers,TheGreatBritishBookShop,Bol,RMBooks,Ebrary,Ebooks,Feedbooks,BookPeople,FNAC,ElCorteIngles,CasaDelLibro,Flipside,AsiaBooks,Litres,PeruBookStore,Gandhi,Livria,Saraiva,Grammata
