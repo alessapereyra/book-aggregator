ecca268063317840cc3a792a1c5bbe8c367cb227
Sascha Reinstein <sascha206@hotmail.com>
2016-03-09 19:58:00 +0000

    Changes item for 24Symbols

--- app/models/symbols.rb
+++ app/models/symbols.rb
@@ -3,7 +3,7 @@ class Symbols < BookStore
   def initialize(value)
     @base_url   = "https://www.24symbols.com/editions/search"
     @options    = { "utf8" => "%E2%9C%93&sb-suggestion=", "search" => value }
-    @results    = ".book.uppercase.masonry-brick"
+    @results    = ".book.uppercase"
     @item       = '.cover a'
   end
 
