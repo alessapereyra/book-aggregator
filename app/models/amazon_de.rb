70328c6897efd461d075a051976e9546cefc58e2
Sascha Reinstein <sascha206@hotmail.com>
2016-03-10 14:48:56 +0000

    Now

--- app/models/amazon_de.rb
+++ app/models/amazon_de.rb
@@ -1,7 +1,7 @@
 class AmazonDE < BookStore
 
   def initialize(value)
-    @base_url   = "http://www.amazon.de/s/ref=nb_sb_noss/280-5316211-4898123"
+    @base_url   = "http://www.amazon.de/s/ref=nb_sb_noss/"
     @options    = {"__mk_de_DE" => "%C3%85M%C3%85%C5%BD%C3%95%C3%91", "url" =>"search-alias%3Daps", "field-keywords" => value }
     @results    = ".s-item-container"
     @item       = '.a-link-normal.s-access-detail-page.a-text-normal'
